6a770f5af60d990d4232c24a0f4f85b33df25dba
Rob Wiscount <robertwiscount@gmail.com>
2021-03-02 15:11:10 -0500

    Add files via upload

---
+++ College Help/Training Projects/Simple C# Projects & Tutorials/Getting Numbers from Text Boxes/Getting Numbers from Text Boxes/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Getting_Numbers_from_Text_Boxes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbFirstNumber = new System.Windows.Forms.TextBox();
+            this.btnAnswer = new System.Windows.Forms.Button();
+            this.tbSecondNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbFirstNumber
+            // 
+            this.tbFirstNumber.Location = new System.Drawing.Point(40, 35);
+            this.tbFirstNumber.Name = "tbFirstNumber";
+            this.tbFirstNumber.Size = new System.Drawing.Size(50, 20);
+            this.tbFirstNumber.TabIndex = 0;
+            this.tbFirstNumber.Text = "10";
+            this.tbFirstNumber.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnAnswer
+            // 
+            this.btnAnswer.Location = new System.Drawing.Point(90, 90);
+            this.btnAnswer.Name = "btnAnswer";
+            this.btnAnswer.Size = new System.Drawing.Size(75, 25);
+            this.btnAnswer.TabIndex = 1;
+            this.btnAnswer.Text = "Answer";
+            this.btnAnswer.UseVisualStyleBackColor = true;
+            this.btnAnswer.Click += new System.EventHandler(this.btnAnswer_Click);
+            // 
+            // tbSecondNumber
+            // 
+            this.tbSecondNumber.Location = new System.Drawing.Point(165, 35);
+            this.tbSecondNumber.Name = "tbSecondNumber";
+            this.tbSecondNumber.Size = new System.Drawing.Size(50, 20);
+            this.tbSecondNumber.TabIndex = 2;
+            this.tbSecondNumber.Text = "5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbSecondNumber);
+            this.Controls.Add(this.btnAnswer);
+            this.Controls.Add(this.tbFirstNumber);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbFirstNumber;
+        private System.Windows.Forms.Button btnAnswer;
+        private System.Windows.Forms.TextBox tbSecondNumber;
+    }
+}
+
---
+++ College Help/Training Projects/Simple C# Projects & Tutorials/Getting Numbers from Text Boxes/Getting Numbers from Text Boxes/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Getting_Numbers_from_Text_Boxes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAnswer_Click(object sender, EventArgs e)
+        {
+
+            int firstTextBoxNumber;
+            int secondTextBoxNumber;
+            int answer;
+
+            firstTextBoxNumber = int.Parse(tbFirstNumber.Text);
+            secondTextBoxNumber = int.Parse(tbSecondNumber.Text);
+
+            answer = firstTextBoxNumber + secondTextBoxNumber;
+
+            MessageBox.Show(answer.ToString());
+
+        }
+    }
+}
